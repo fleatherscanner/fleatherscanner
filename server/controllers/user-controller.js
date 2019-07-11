a1d0652286c1193f22b5a614ac637c4459cf4f49
Filbert <filberttt@hotmail.com>
2019-07-11 18:03:07 +0700

    starting

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -1,18 +1,26 @@
 const {OAuth2Client} = require('google-auth-library');
 const CLIENT_ID = process.env.GOOGLE_CLIENT_ID
 const client = new OAuth2Client(CLIENT_ID);
+const User = require("../models/user")
 
 
 class UserController {
     static googleSignin (req, res, next) {
+        var payload = null
         client.verifyIdToken({
             idToken: req.body.token,
             audience: CLIENT_ID
         })
         .then(ticket =>{
-            const payload = ticket.getPayload()
-            console.log(payload)
-            res.status(200).json(payload)
+            payload = ticket.getPayload()
+            return User.findOne({email: payload.email})
+        })
+        .then(user =>{
+            if(user){
+
+            } else {
+
+            }
         })
         .catch(next)
     }
