9b025da62cbe22c14565d9f9055a71d4c0f157c1
Filbert <filberttt@hotmail.com>
2019-07-11 17:54:13 +0700

    improved google sigin

--- client/main.js
+++ client/main.js
@@ -3,8 +3,22 @@ $(document).ready(function(){
     $('.modal').modal();
 });
 
+const baseUrl = "http://localhost:3000/api"
+
 
 function onSignIn(googleUser) {
     var id_token = googleUser.getAuthResponse().id_token;
-    console.log(id_token)
+    $.ajax({
+        method: "POST",
+        url: `${baseUrl}/users/signin/google`,
+        data: {
+            token: id_token
+        }
+    })
+    .done(function(response) {
+        console.log(response)
+    })
+    .fail(function(err) {
+        console.log(err)
+    })
   }
--- server/routes/user-route.js
+++ server/routes/user-route.js
@@ -2,7 +2,9 @@ const express = require("express")
 const router = express.Router()
 const userController = require("../controllers/user-controller")
 
-
+router.post("/signin/google", userController.googleSignin)
+router.post("/signin", userController.login)
+router.post("/signup", userController.register)
 
 
 module.exports = router
