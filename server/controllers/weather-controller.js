0c32bbef403ab1109bd167d2a92884f7cf37d3dd
Filbert <filberttt@hotmail.com>
2019-07-12 09:28:48 +0700

    update 1

---
+++ server/controllers/weather-controller.js
@@ -0,0 +1,26 @@
+const baseUrl = "https://www.metaweather.com/api/location"
+const axios = require("axios")
+
+class WeatherController {
+    
+    static getWeather(req, res, next) {
+        const {city, year, month, date} = req.params
+        axios.request({
+            method: "GET",
+            url: `${baseUrl}/search/?query=${city}`
+        })
+        .then(response =>{
+            var woeid = response.data[0].woeid
+            return axios.request({
+                method: "GET",
+                url: `${baseUrl}/${woeid}/${year}/${month}/${date}`
+            })
+        })
+        .then(response =>{
+            res.status(200).json(response.data[0])
+        })
+        .catch(next)
+    }
+}
+
+module.exports = WeatherController
--- server/routes/flight-route.js
+++ server/routes/flight-route.js
@@ -1,10 +1,13 @@
 const express = require("express")
 const router = express.Router()
 const FlightController = require("../controllers/flight-controller")
+const WeatherController = require("../controllers/weather-controller")
 
 router.post("/flight", (req, res) => {
     res.send("It works!")
 })
 router.post("/flight", FlightController.searchRoute)
+
+router.get("/weather/:city/:year/:month/:date", WeatherController.getWeather)
 
 module.exports = router
